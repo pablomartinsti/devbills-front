6118b58cbe7c6e9926904428c77700f84b351dee
pablomartinsti <pablomartinsti@outlook.com>
2024-10-30 14:12:52 -0300

    Componente Card

---
+++ src/screens/home/index.tsx
@@ -0,0 +1,51 @@
+import { InputMask } from '@react-input/mask';
+import { Button } from '../../components/button';
+import { Input } from '../../components/input';
+import { Logo } from '../../components/logo';
+import { Title } from '../../components/title';
+import { Header, Main, Section, Filters, InputGroup, Balance } from './styles';
+import { ButtonIcon } from '../../components/button-icon';
+import { Card } from '../../components/card';
+
+export function Home() {
+  return (
+    <>
+      <Header>
+        <Logo />
+        <div>
+          <Button>Nova Transação</Button>
+          <Button>Nova Categoria</Button>
+        </div>
+      </Header>
+      <Main>
+        <Section>
+          <Filters>
+            <Title title="Saldo" subtitle="Receitas e despesas no período" />
+            <InputGroup>
+              <InputMask
+                component={Input}
+                mask="dd/mm/yyyy"
+                replacement={{ d: /\d/, m: /\d/, y: /\d/ }}
+                label="Início"
+                placeholder="dd/mm/yyyy"
+              />
+              <InputMask
+                component={Input}
+                mask="dd/mm/yyyy"
+                replacement={{ d: /\d/, m: /\d/, y: /\d/ }}
+                label="Fim"
+                placeholder="dd/mm/yyyy"
+              />
+              <ButtonIcon />
+            </InputGroup>
+          </Filters>
+          <Balance>
+            <Card title="Saldo" amount={1000000} />
+            <Card title="Saldo" amount={1000000} variant="incomes" />
+            <Card title="Saldo" amount={1000000} variant="expenses" />
+          </Balance>
+        </Section>
+      </Main>
+    </>
+  );
+}
