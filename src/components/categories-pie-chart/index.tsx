20f4b57cdadcb42c3648efe5677105db2c611aa7
pablomartinsti <pablomartinsti@outlook.com>
2024-11-01 10:02:57 -0300

    Gráfico de Gastos pt.2

--- src/components/categories-pie-chart/index.tsx
+++ src/components/categories-pie-chart/index.tsx
@@ -1,6 +1,7 @@
 import { ResponsivePie } from '@nivo/pie';
 import { useMemo } from 'react';
 import { theme } from '../../styles/theme';
+import { formatCurrency } from '../../utils/format-currency';
 
 const apiData = [
   {
@@ -48,7 +49,8 @@ export function CategoriesPieChart() {
       enableArcLabels={false}
       enableArcLinkLabels={false}
       colors={({ data }) => data.color}
-      margin={{ top: 8 }}
+      margin={{ top: 20 }}
+      valueFormat={formatCurrency}
       theme={{
         text: { fontFamily: 'Lexend', fontSize: 10 },
         tooltip: {
@@ -68,7 +70,7 @@ export function CategoriesPieChart() {
           direction: 'row',
           justify: false,
           translateX: 0,
-          translateY: -28,
+          translateY: -20,
           itemWidth: 120,
           itemHeight: 16,
           itemTextColor: theme.colors.neutral,
